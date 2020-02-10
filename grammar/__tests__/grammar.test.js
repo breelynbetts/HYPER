783cd13247a858a68953af261d83edc40483ce50
Breelyn Betts <breelynbetts@gmail.com>
2020-02-09 22:36:56 -0800

    3/3 tests are passing

--- grammar/__tests__/grammar.test.js
+++ grammar/__tests__/grammar.test.js
@@ -6,16 +6,54 @@
 const syntaxCheck = require('../syntax-checker');
 
 const program = String.raw`
-INT x!
-LOOKAT INT x IN range(0,10): 
-⇨ SAY x!
+FUNC INT fibonacci( INT number ): 
+⇨ TRY num EQUALS 0: 
+	⇨ GIMME 0! 
+⇦ NO?TRY num EQUALS 1 OR num EQUALS 2:
+⇨ GIMME 1!
+⇦ SAY fibonacci(num SUB 2) ADD fibonacci(num SUB 1)!
 ⇦
+`;
+const program2 = String.raw`
+INT num1 IS 10!
+INT num2 IS 14!
+INT num3 IS 12!
+INT largest!
+
+TRY (num1 GRTEQ num2) AND (num1 GRTEQ num3):
+⇨largest IS num1!
+⇦NO?TRY (num2 GRTEQ num1) AND (num2 GRTEQ num3):
+⇨largest IS num2!
+⇦NO???:
+ ⇨largest IS num3!
+⇦SAY("The largest number is ", largest)! 
+`;
+
+const program3 = String.raw`
+FUNC BOO isEven(INT num):
+⇨ GIMME num MOD 2 EQUALS 0!
+⇦ 
 `;
 
 
+
 describe('The syntax checker', () => {
-  test('accepts the mega program with all syntactic forms', (done) => {
-    expect(syntaxCheck(program)).toBe(true);
-    done();
+    test('accepts the fibonacci function', (done) => {
+        expect(syntaxCheck(program)).toBe(true);
+        done();
   });
 });
+
+describe('The syntax checker', () => {
+    test('accepts the returns greatest number function', (done) => {
+        expect(syntaxCheck(program2)).toBe(true);
+        done();
+    });
+  });
+
+describe('The syntax checker', () => {
+    test('accepts the returns greatest number function', (done) => {
+        expect(syntaxCheck(program3)).toBe(true);
+        done();
+    });
+  });
