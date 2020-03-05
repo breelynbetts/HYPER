59ea7c7d7eef3d56d1844dccaa74ea265135def3
Breelyn Betts <breelynbetts@gmail.com>
2020-03-05 15:15:29 -0800

    prettified files

--- ast/__tests__/parser.test.js
+++ ast/__tests__/parser.test.js
@@ -1,27 +1,47 @@
-const parse = require('../parser');
+const parse = require("../parser");
 
 const {
-    Program, ForStatement, WhileStatement, IfStatement, Function,
-    Assignment, Declaration, PrintStatement,ReturnStatement, Break,
-    Block, BinaryExp, UnaryExp, ArrayExp, DictExp, TupleExp, CallExp,
-    RangeExp, MemberExp, SubscriptedExp, IdExp, Param, Literal, Identifier
-} = require('../../ast');
+  Program,
+  ForStatement,
+  WhileStatement,
+  IfStatement,
+  Function,
+  Assignment,
+  Declaration,
+  PrintStatement,
+  ReturnStatement,
+  Break,
+  Block,
+  BinaryExp,
+  UnaryExp,
+  ArrayExp,
+  DictExp,
+  TupleExp,
+  CallExp,
+  RangeExp,
+  MemberExp,
+  SubscriptedExp,
+  IdExp,
+  Param,
+  Literal,
+  Identifier
+} = require("../../ast");
 
 const fixture = {
-    hello: [
-        String.raw`SAY "hello!\n"`,
-        new CallExp('SAY', [new Literal('hello!\\n')]), 
-    ],
-} 
+  hello: [
+    String.raw`SAY "hello!\n"`,
+    new CallExp("SAY", [new Literal("hello!\\n")])
+  ]
+};
 
-describe('The parser', () => {
-    Object.entries(fixture).forEach(([name, [source, expected]]) => {
-        test(`produces the correct AST for ${name}`, (done) => {
-            expect(parse(source)).toEqual(expected);
-        });
+describe("The parser", () => {
+  Object.entries(fixture).forEach(([name, [source, expected]]) => {
+    test(`produces the correct AST for ${name}`, done => {
+      expect(parse(source)).toEqual(expected);
     });
-    test('throws an exception on a syntax error', (done) => {
-        expect(() => parse('as$df^&%*$&')).toThrow();
-        done();
-    });
+  });
+  test("throws an exception on a syntax error", done => {
+    expect(() => parse("as$df^&%*$&")).toThrow();
+    done();
+  });
 });
--- grammar/__tests__/grammar_errors.test.js
+++ grammar/__tests__/grammar_errors.test.js
@@ -1,22 +1,25 @@
-/* 
+/*
  *  Grammar Error Checker
- *   
+ *
  */
-const syntaxCheck = require('../syntax-checker');
+const syntaxCheck = require("../syntax-checker");
 
 const errors = [
-    ["ambigous operations ", "INT x IS 5++ ** 7!"],
-    ["forgotten ! at end of line ", "STR x IS \"Hyper\""],
-    ["no type declaration of FUNC",  `FUNC isEven(INT num):
+  ["ambigous operations ", "INT x IS 5++ ** 7!"],
+  ["forgotten ! at end of line ", 'STR x IS "Hyper"'],
+  [
+    "no type declaration of FUNC",
+    `FUNC isEven(INT num):
     ⇨ GIMME num MOD 2 EQUALS 0!
-    ⇦`]
-]
+    ⇦`
+  ]
+];
 
 describe("The syntax checker", () => {
-    errors.forEach(([error, example]) => {
-      test(`detects the error ${error}`, done => {
-        expect(syntaxCheck(example)).toBe(false);
-        done();
-      });
+  errors.forEach(([error, example]) => {
+    test(`detects the error ${error}`, done => {
+      expect(syntaxCheck(example)).toBe(false);
+      done();
     });
   });
+});
