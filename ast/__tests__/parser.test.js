c0f6d1b06fc6bd9a1b44dba5fdfb57beaeb54dab
Lexi Weingardt <aweingar@lion.lmu.edu>
2020-03-07 14:51:59 -0800

    added tests to parser test

--- ast/__tests__/parser.test.js
+++ ast/__tests__/parser.test.js
@@ -34,123 +34,138 @@ let fixture = {
     new Program([new PrintStatement(new Literal(3))])
   ]
 
-  // for: [
-  //   String.raw`LOOKAT INT x IN RANGE(0, 10):`,
-  //   new ForStatement(
-  //     "i",
-  //     new Literal(0),
-  //     new Literal(9),
-  //     new IfExp(
-  //       new IdExp("i"),
-  //       new Assignment(new IdExp("i"), new Literal(100)),
-  //       null
-  //     )
-  //   )
-  // ],
+    assignment: [
+    String.raw`x IS x + 3`,
+    new Assignment("SAY", [new Literal("hello!\\n")])
+  ],
 
-  // while: [
-  //   String.raw`UNTIL TRUE:`,
-  //   new ForStatement(
-  //     "i",
-  //     new Literal(0),
-  //     new Literal(9),
-  //     new IfExp(
-  //       new IdExp("i"),
-  //       new Assignment(new IdExp("i"), new Literal(100)),
-  //       null
-  //     )
-  //   )
-  // ],
+  declaration: [
+    String.raw`x IS 5`,
+    new Declaration("SAY", [new Literal("hello!\\n")])
+  ],
 
-  // if: [
-  //   String.raw`TRY 4 GRT 0:`,
-  //   new ForStatement(
-  //     "i",
-  //     new Literal(0),
-  //     new Literal(9),
-  //     new IfExp(
-  //       new IdExp("i"),
-  //       new Assignment(new IdExp("i"), new Literal(100)),
-  //       null
-  //     )
-  //   )
-  // ],
+  print: [
+    String.raw`SAY "Hello World!\n"`,
+    new PrintStatement("SAY", [new Literal("Hello World!\\n")])
+  ],
 
-  // arrays: [
-  //   String.raw`ARR c IS ["Hi", "I", "am", "hyper"]`,
-  //   new LetExp(
-  //     [
-  //       new TypeDec("list", new ArrayType("int")),
-  //       new Variable(
-  //         "x",
-  //         "list",
-  //         new ArrayExp("list", new Literal(1), new NegationExp(new Literal(9)))
-  //       )
-  //     ],
-  //     [new SubscriptedExp(new IdExp("x"), new Literal(0))]
-  //   )
-  // ],
+  returnStatement: [
+    String.raw`FUNC BOO isEven(INT num):
+    GIMME num MOD 2 EQUALS 0!`,
+    new CallExp("SAY", [new Literal("hello!\\n")])
+  ],
 
-  // dict: [
-  //   String.raw`DICT e IS {1: "Hi", 2: "I", 3: "am", 4: "hyper"}`,
-  //   new LetExp(
-  //     [
-  //       new TypeDec("list", new ArrayType("int")),
-  //       new Variable(
-  //         "x",
-  //         "list",
-  //         new ArrayExp("list", new Literal(1), new NegationExp(new Literal(9)))
-  //       )
-  //     ],
-  //     [new SubscriptedExp(new IdExp("x"), new Literal(0))]
-  //   )
-  // ],
+  binary: [
+    String.raw`INT x IS 3
+    INT y IS 5
+    INT z IS x ADD y`,
+    new BinaryExp("SAY", [new Literal("hello!\\n")])
+  ],
 
-  // tuple: [
-  //   String.raw`TUP d IS (1, 2.5, "hello")`,
-  //   new LetExp(
-  //     [
-  //       new TypeDec("list", new ArrayType("int")),
-  //       new Variable(
-  //         "x",
-  //         "list",
-  //         new ArrayExp("list", new Literal(1), new NegationExp(new Literal(9)))
-  //       )
-  //     ],
-  //     [new SubscriptedExp(new IdExp("x"), new Literal(0))]
-  //   )
-  // ],
+  unary: [
+    String.raw`INT x IS -10`,
+    new UnaryExp("SAY", [new Literal("hello!\\n")])
+  ],
 
-  // range: [
-  //   String.raw`INT x IN RANGE(0, 10):)`,
-  //   new LetExp(
-  //     [
-  //       new TypeDec("list", new ArrayType("int")),
-  //       new Variable(
-  //         "x",
-  //         "list",
-  //         new ArrayExp("list", new Literal(1), new NegationExp(new Literal(9)))
-  //       )
-  //     ],
-  //     [new SubscriptedExp(new IdExp("x"), new Literal(0))]
-  //   )
-  // ],
+  for: [
+    String.raw`LOOKAT INT x IN RANGE(0, 10):
+    SAY x!`,
+    new ForStatement(
+      'i',
+      new Literal(0),
+      new Literal(9),
+      new IfExp(new IdExp('i'), new Assignment(new IdExp('i'), new Literal(100)), null),
+    ),
+  ],
 
-  // simpleFunction: [
-  //   String.raw`FUNC BOO isEven(INT num):
-  //   GIMME num MOD 2 EQUALS 0!`,
-  //   new LetExp(
-  //     [
-  //       new Func(
-  //         "addTwo",
-  //         [new Param("x", "int")],
-  //         "int",
-  //         new BinaryExp("+", new IdExp("x"), new Literal(2))
-  //       )
-  //     ],
-  //     [new Call("addTwo", [new Call("ord", [new Literal("dog")])])]
-  //   )
-  // ]
+  while: [
+    String.raw`UNTIL TRUE:
+    SAY "I am hyper!"!`,
+    new WhileStatement(
+      'i',
+      new Literal(0),
+      new Literal(9),
+      new IfExp(new IdExp('i'), new Assignment(new IdExp('i'), new Literal(100)), null),
+    ),
+  ],
+
+  if: [
+    String.raw`INT num IS 3!
+    TRY num GRT 0:
+      SAY "Positive number"!
+    NO?TRY num EQUALS 0:
+      SAY "Zero"!
+    NO???:
+      SAY "Negative number"!`,
+    new IfStatement(
+      'i',
+      new Literal(0),
+      new Literal(9),
+      new IfExp(new IdExp('i'), new Assignment(new IdExp('i'), new Literal(100)), null),
+    ),
+  ],
+
+  arrays: [
+    String.raw`ARR c IS ["Hi", "I", "am", "hyper"]
+    SAY c!`,
+    new ArrayExp(
+      [
+        new TypeDec('list', new ArrayType('int')),
+        new Variable('x', 'list',
+          new ArrayExp('list', new Literal(1), new NegationExp(new Literal(9)))),
+      ],
+      [new SubscriptedExp(new IdExp('x'), new Literal(0))],
+    ),
+  ],
+
+  dict: [
+    String.raw`DICT e IS {1: "Hi", 2: "I", 3: "am", 4: "hyper"}
+    SAY e!`,
+    new DictExp(
+      [
+        new TypeDec('list', new ArrayType('int')),
+        new Variable('x', 'list',
+          new ArrayExp('list', new Literal(1), new NegationExp(new Literal(9)))),
+      ],
+      [new SubscriptedExp(new IdExp('x'), new Literal(0))],
+    ),
+  ],
+
+  tuple: [
+    String.raw`TUP d IS (1, 2.5, "hello")
+    SAY d!`,
+    new TupleExp(
+      [
+        new TypeDec('list', new ArrayType('int')),
+        new Variable('x', 'list',
+          new ArrayExp('list', new Literal(1), new NegationExp(new Literal(9)))),
+      ],
+      [new SubscriptedExp(new IdExp('x'), new Literal(0))],
+    ),
+  ],
+
+  range: [
+    String.raw`LOOKAT INT x IN RANGE(0, 10):
+    SAY x!`,
+    new RangeExp(
+      [
+        new TypeDec('list', new ArrayType('int')),
+        new Variable('x', 'list',
+          new ArrayExp('list', new Literal(1), new NegationExp(new Literal(9)))),
+      ],
+      [new SubscriptedExp(new IdExp('x'), new Literal(0))],
+    ),
+  ],
+
+  simpleFunction: [
+    String.raw`FUNC BOO isEven(INT num):
+    GIMME num MOD 2 EQUALS 0!`,
+    new Function(
+      [new Func('isEven', [new Param('x', 'int')], 'int',
+        new BinaryExp('+', new IdExp('x'), new Literal(2)))],
+      [new Call('isEven', [new Call('ord', [new Literal('dog')])])],
+    ),
+  ],
 };
 
 describe("The parser", () => {
