f8278201a4f1e3a8c08b36bf0acc6b59d5c6146d
Breelyn Betts <breelynbetts@gmail.com>
2020-04-01 18:19:38 -0700

    added methods to checker

--- semantics/check.js
+++ semantics/check.js
@@ -1,5 +1,11 @@
 const util = require("util");
-const { ArrayType, Function, RecordType, Identifier } = require("../ast");
+const {
+  Function,
+  Identifier,
+  ArrayType,
+  DictType,
+  TupleType
+} = require("../ast");
 const {
   BoolType,
   FloatType,
@@ -14,4 +20,41 @@ function doCheck(condition, message) {
   }
 }
 
-module.exports = {};
+module.exports = {
+  isArrayType(type) {
+    doCheck(type.constructor === ArrayType, "Not an ArrayType");
+  },
+  isDictType(type) {
+    doCheck(type.constructor === DictType, "Not a DictType");
+  },
+  isTupleType(type) {
+    doCheck(type.constructor === TupleType, "Not a TupleType");
+  },
+  isArray(expression) {
+    doCheck(expression.type.constructor === ArrayType, "Not an array");
+  },
+  isDict(expression) {
+    doCheck(expression.type.constructor === DictType, "Not a dictionary");
+  },
+  isTuple(expression) {
+    doCheck(expression.type.constructor === TupleType, "Not a tuple");
+  },
+  isInteger(expression) {
+    doCheck(expression.type === IntType, "Not an integer");
+  },
+  isFloat(expression) {
+    doCheck(expression.type === FloatType, "Not a float");
+  },
+  isNumber(expression) {
+    doCheck(
+      expression.type === IntType || expression.type === FloatType,
+      "Not a number"
+    );
+  },
+  isString(expression) {
+    doCheck(expression.type === StringType, "Not an string");
+  },
+  isBoolean(expression) {
+    doCheck(expression.type === BoolType, "Not a boolean");
+  }
+};
